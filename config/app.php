b5f7134fdc067c0efe1af3f7debfc3aaf7a141e7
Raju Malla <irwifi@gmail.com>
2015-12-11 17:24:19 +0545

    Initial configuration.

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,7 +39,9 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    // Default Value
+    // 'timezone' => 'UTC',
+    'timezone' => 'Asia/Kathmandu',
 
     /*
     |--------------------------------------------------------------------------
